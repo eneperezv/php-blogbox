341d5b51e186cdbfa2b46270d32f22e014f8c24b
eneperezv <eneperezv@gmail.com>
2024-12-01 19:42:51 -0500

    Inclusion de documentacion inicial de las clases
     Changes to be committed:
    	modified:   api/src/Controllers/RoutesController.php
    	modified:   api/src/Models/User.php

--- api/src/Models/User.php
+++ api/src/Models/User.php
@@ -20,49 +20,16 @@ class User{
     public static function findByEmail($email) {
         $result = UserDal::findByEmail($email);
         return $result === false ? [] : $result;
-        /*
-        $db = Connection::connect();
-        
-        $stmt = $db->prepare("SELECT * FROM users WHERE email = :email");
-        $stmt->bindParam(":email", $email, \PDO::PARAM_STR);
-        $stmt->execute();
-
-        return $stmt->fetch(\PDO::FETCH_ASSOC);
-        */
     }
 
     public static function findById($userId) {
         $result = UserDal::findById($userId);
         return $result === false ? [] : $result;
-        /*
-        $db = Connection::connect();
-        
-        $stmt = $db->prepare("SELECT * FROM users WHERE id = :userId");
-        $stmt->bindParam(":userId", $userId, \PDO::PARAM_STR);
-        $stmt->execute();
-
-        return $stmt->fetch(\PDO::FETCH_ASSOC);
-        */
     }
 
     public static function create($data, $password) {
         $result = UserDal::create($userId);
         return $result === false ? [] : $result;
-        /*
-        $db = Connection::connect();
-
-        $stmt = $db->prepare("INSERT INTO users (email, password, role, name, phone, timezone) 
-                              VALUES (:email, :password, :role, :name, :phone, :timezone)");
-
-        $stmt->bindParam(":email", $data['email'], \PDO::PARAM_STR);
-        $stmt->bindParam(":password", $password, \PDO::PARAM_STR);
-        $stmt->bindParam(":role", $data['role'], \PDO::PARAM_STR);
-        $stmt->bindParam(":name", $data['name'], \PDO::PARAM_STR);
-        $stmt->bindParam(":phone", $data['phone'], \PDO::PARAM_STR);
-        $stmt->bindParam(":timezone", $data['timezone'], \PDO::PARAM_STR);
-        
-        return $stmt->execute();
-        */
     }
 
 }
