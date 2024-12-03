9edad31df56f44875d71dbc7e2c2510b231700c1
eneperezv <eneperezv@gmail.com>
2024-12-02 22:32:37 -0500

    Creacion de metodos para getByAuthor, getAll y create en PostController
     Changes to be committed:
    	modified:   api/src/Controllers/PostController.php

--- api/src/Controllers/PostController.php
+++ api/src/Controllers/PostController.php
@@ -28,7 +28,7 @@ class PostController{
             $postDetails = Post::findById($postId);
 
             if ($postDetails) {
-                return Response::success('Consulta de datos', $postDetails, 201, 'service');
+                return Response::success('Consulta de datos', $postDetails, 200, 'post');
             } else {
                 $err = array('error' => 'Error al consultar el post.');
                 return Response::error('Ha ocurrido un error en la solicitud.', $err, 422);
@@ -41,14 +41,66 @@ class PostController{
     }
 
     public static function findByAuthorId($authorId) {
+
+        $errors = Validator::validateConsultaPorId($authorId);
+        if (!empty($errors)) {
+            return Response::error('Ha ocurrido un error en la solicitud.', $errors, 422);
+        }
+
+        try {
+            $postDetails = Post::findByAuthorId($authorId);
+
+            if ($postDetails) {
+                return Response::success('Consulta de datos', $postDetails, 200, 'post');
+            } else {
+                $err = array('error' => 'Error al consultar posts por autor.');
+                return Response::error('Ha ocurrido un error en la solicitud.', $err, 422);
+            }
+        } catch (\Exception $e) {
+            $err = array('error' => 'Error en el servidor: ' . $e->getMessage());
+            return Response::error('Error en el servidor', $err, 500);
+        }
         
     }
 
     public static function findAll() {
+
+        try {
+            $postDetails = Post::findAll();
+
+            if ($postDetails) {
+                return Response::success('Consulta de datos', $postDetails, 200, 'post');
+            } else {
+                $err = array('error' => 'Error al consultar posts.');
+                return Response::error('Ha ocurrido un error en la solicitud.', $err, 422);
+            }
+        } catch (\Exception $e) {
+            $err = array('error' => 'Error en el servidor: ' . $e->getMessage());
+            return Response::error('Error en el servidor', $err, 500);
+        }
         
     }
 
     public static function create($data) {
+
+        $errors = Validator::validateDataPost($data);
+        if (!empty($errors)) {
+            return Response::error('Ha ocurrido un error en la solicitud.', $errors, 422);
+        }
+
+        try {
+            $postDetails = Post::create($data);
+
+            if ($postDetails) {
+                return Response::success('Post creado con éxito.', $postDetails, 200, 'post');
+            } else {
+                $err = array('error' => 'Error al registrar el post.');
+                return Response::error('Ha ocurrido un error en la solicitud.', $err, 422);
+            }
+        } catch (\Exception $e) {
+            $err = array('error' => 'Error en el servidor: ' . $e->getMessage());
+            return Response::error('Error en el servidor', $err, 500);
+        }
         
     }
 
