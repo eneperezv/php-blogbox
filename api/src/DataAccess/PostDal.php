284d5b93c669424d0e590dfe7933cb41a92cb3ce
eneperezv <eneperezv@gmail.com>
2024-12-13 17:31:08 -0500

    Optimizacion de codigo
     Changes to be committed:
    	modified:   api/logs/app.log
    	modified:   api/src/DataAccess/PostDal.php

--- api/src/DataAccess/PostDal.php
+++ api/src/DataAccess/PostDal.php
@@ -25,7 +25,7 @@ class PostDal{
                                 (SELECT COUNT(*) FROM votes WHERE votes.post_id = posts.id AND votes.option = 'DOWN') AS downvote_count,
                                 (SELECT COUNT(*) FROM comments WHERE comments.post_id = posts.id) AS comment_count
                             FROM posts
-                            LEFT JOIN users ON posts.author_id = users.id WHERE posts.id = :postId");
+                            LEFT JOIN users ON posts.author_id = users.id WHERE posts.id = :postId ORDER BY posts.id DESC");
         $stmt->bindParam(":postId", $postId, \PDO::PARAM_INT);
         $stmt->execute();
 
@@ -42,7 +42,7 @@ class PostDal{
                                 (SELECT COUNT(*) FROM votes WHERE votes.post_id = posts.id AND votes.option = 'DOWN') AS downvote_count,
                                 (SELECT COUNT(*) FROM comments WHERE comments.post_id = posts.id) AS comment_count
                             FROM posts
-                            LEFT JOIN users ON posts.author_id = users.id WHERE posts.author_id = :author_id");
+                            LEFT JOIN users ON posts.author_id = users.id WHERE posts.author_id = :author_id ORDER BY posts.id DESC");
         $stmt->bindParam(":author_id", $authorId, \PDO::PARAM_INT);
         $stmt->execute();
 
@@ -59,7 +59,7 @@ class PostDal{
                                 (SELECT COUNT(*) FROM votes WHERE votes.post_id = posts.id AND votes.option = 'DOWN') AS downvote_count,
                                 (SELECT COUNT(*) FROM comments WHERE comments.post_id = posts.id) AS comment_count
                             FROM posts
-                            LEFT JOIN users ON posts.author_id = users.id");
+                            LEFT JOIN users ON posts.author_id = users.id ORDER BY posts.id DESC");
         $stmt->execute();
 
         $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
