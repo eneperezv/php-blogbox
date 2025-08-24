fad127958ff93824adbb8b19736630e4411f31e9
eneperezv <eneperezv@gmail.com>
2025-08-23 23:12:14 -0500

    Revision de documentación de clases.
     Changes to be committed:
    	modified:   cli/includes/app.php
    	modified:   cli/includes/author.php
    	modified:   cli/includes/footer.php
    	modified:   cli/includes/functions.php
    	modified:   cli/includes/header.php
    	modified:   cli/includes/home.php
    	modified:   cli/includes/login.php
    	modified:   cli/includes/post.php
    	modified:   cli/index.php

--- cli/includes/header.php
+++ cli/includes/header.php
@@ -4,6 +4,6 @@
         <li class="nav-item"><a href="index.php?action=logout" class="nav-link">Salir</a></li>
         <!-- <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
         <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li> -->
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li> -->  
     </ul>
 </header>
